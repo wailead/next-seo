f40208a2efd5dcd461c9a864f75e658927bd4ed8
lumpen <swk5373@gmail.com>
2024-12-02 12:08:57 +0900

    📬 Chore: globals.css 설정

--- wairi/tailwind.config.ts
+++ wairi/tailwind.config.ts
@@ -11,6 +11,29 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary: {
+          default: "#2EC8C8",
+          100: "#E0F5F6",
+          200: "#C3F6F6",
+        },
+
+        gray: {
+          default: "#222222",
+          100: "#BDBDBD",
+          200: "#9E9E9E",
+          300: "#616161",
+          400: "#5C5C5C",
+          500: "#4D4D4D",
+          600: "#424242",
+        },
+        white: {
+          default: "#FFFFFF",
+          100: "#F9F9F9",
+          200: "#F5F5F5",
+          300: "#F2F2F2",
+          400: "#EEEEEE",
+          500: "#D9D9D9",
+        }
       },
     },
   },
