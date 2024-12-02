5f934c0c1217ff8527eacf6ed79b31fe1a14a19d
lumpen <swk5373@gmail.com>
2024-12-02 15:04:13 +0900

    ✨ Feature: Header, Footer 세팅

--- src/app/(route)/layout.tsx
+++ src/app/(route)/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import { Header, Footer } from "@/components";
 import "../styles/globals.css";
 
 const geistSans = localFont({
@@ -28,7 +29,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
