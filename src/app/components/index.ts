bdcda786b0a2a1a07c439eb64712fb56da78f753
양혜지 <97512098+hyeji17@users.noreply.github.com>
2024-12-03 17:01:52 +0900

    ✨ Feature: title-form 컴포넌트 추가

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -2,5 +2,6 @@ import PcHeader from "./header/header_pc";
 import Footer from "./footer/footer";
 import Button from "./button/button";
 import MobileHeader from "./header/header_mobile";
+import TitleForm from "./titleForm/titleForm";
 
-export { PcHeader, Footer, Button, MobileHeader };
+export { PcHeader, Footer, Button, MobileHeader, TitleForm };
