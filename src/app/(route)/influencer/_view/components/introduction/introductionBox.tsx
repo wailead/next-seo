ee6a4a2ec90387b41635e5b30b7db9fa848c5e1d
양혜지 <97512098+hyeji17@users.noreply.github.com>
2024-12-04 13:23:01 +0900

    Feat: Introduction 컴포넌트 UI 구현 #19

--- src/app/(route)/influencer/_view/components/introduction/introductionBox.tsx
+++ src/app/(route)/influencer/_view/components/introduction/introductionBox.tsx
@@ -11,8 +11,8 @@ function IntroductionBox({ className, title, description, children }: childrenPr
   return (
     <div className={`${className} rounded-[16.64px]`}>
       <div className="flex flex-col gap-[18.2px] pl-[41.6px] pt-[44.72px]">
-        <h1 className="font-pretendard font-bold text-gray-600 text-[24.96px]">{title}</h1>
-        <p className="font-pretendard font-normal text-gray-600 text-[18.72px]">{description}</p>
+        <h1 className="font-pretendard font-bold text-gray-600 text-[24.96px]"  dangerouslySetInnerHTML={{ __html: title }} />
+        <p className="font-pretendard font-normal text-gray-600 text-[18.72px]"  dangerouslySetInnerHTML={{ __html: description }} />
       </div>
       {children}
     </div>
