cc004dfa9de79bb57406ffb1fde389de58b5bc2d
lumpen <swk5373@gmail.com>
2024-12-05 07:09:58 +0900

    ✨ Feat: 마케팅 배너 추가 작업중 #40

---
+++ src/app/(route)/influencer/_view/marketing-banner/marketing-banner.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+function MarketingBanner() {
+  return (
+    <section>
+      <Link href="/influencer/marketing">
+        <h1>무료로 하는 와이리 마케팅 알아보기</h1>
+      </Link>
+    </section>
+  )
+}
+export default MarketingBanner
