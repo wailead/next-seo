3f040f870a0d4ddd11be901f2233e743b797d19c
lumpen <swk5373@gmail.com>
2024-12-05 20:25:45 +0900

    🔨 Refactor: CustomImage, LinkButton 수정 및 적용 #70

--- src/app/(route)/influencer/_view/marketing-banner/MarketingBanner.tsx
+++ src/app/(route)/influencer/_view/marketing-banner/MarketingBanner.tsx
@@ -5,15 +5,20 @@ function MarketingBanner() {
   return (
     <section
       className={
-        'w-full h-[7.5625rem] bg-white-500 text-gray-600 text-[1.375rem] font-[500] flex items-center justify-center'
+        'w-full h-[7.5625rem] bg-white-300 text-gray-600 text-[1.2rem] font-[500] flex items-center justify-center'
       }>
-      <div className="w-primary flex items-center gap-[3.125rem] justify-center">
+      <div className="w-primary flex items-center gap-[2rem] justify-center">
         <div>
           <p>인플루언서 광고가 필요하신가요?</p>
           <Link href="/influencer/marketing">
-            <h1 className="text-primary-default flex">
+            <h1 className="text-primary-default font-semibold flex items-center">
               무료로 하는 와이리 마케팅 알아보기
-              <CustomImage src="/icons/icon_arrow_back.svg" alt="marketing 화살표 아이콘" w="w-[3rem]" h="h-[3rem]" />
+              <CustomImage
+                src="/icons/icon_arrow_back.svg"
+                alt="marketing 화살표 아이콘"
+                w="w-[1.65rem]"
+                h="h-[2rem]"
+              />
             </h1>
           </Link>
         </div>
--- src/app/components/button/LinkButton.tsx
+++ src/app/components/button/LinkButton.tsx
@@ -10,16 +10,28 @@ interface ButtonProps extends React.ComponentPropsWithoutRef<typeof Link> {
 }
 
 function LinkButton({ icon, name, fontColor, buttonColor, border, ...linkProps }: ButtonProps) {
-  const textColor = fontColor === 'white' ? 'text-white' : 'text-primary-default'
+  const textColor = fontColor === 'white' ? 'text-white-default' : 'text-primary-default'
   const linkColor = `bg-${buttonColor}`
-  const borderColor = border ? `border-primary-default border-[2px]` : ''
-  const hoverColor = `hover:bg-${buttonColor}/70`
-  const activeColor = `active:bg-${buttonColor}/60`
+  const borderColor = border ? `border-primary-default border-[3px]` : ''
+  const hoverColor = ((color: string) => {
+    return {
+      'primary-200': 'hover:bg-primary-200/80',
+      'primary-default': 'hover:bg-primary-default/80',
+      'white-default': 'hover:bg-white-default/80',
+    }[color]
+  })(buttonColor)
+  const activeColor = ((color: string) => {
+    return {
+      'primary-200': 'active:bg-primary-200/70',
+      'primary-default': 'active:bg-primary-default/70',
+      'white-default': 'active:bg-white-default/70',
+    }[color]
+  })(buttonColor)
   return (
     <Link
       {...linkProps}
       className={twMerge(
-        `px-[16.64px] py-[6.24px] rounded-[29.12px] flex items-center justify-center gap-[4px]`,
+        `px-[16.64px] py-[7px] rounded-[29.12px] flex items-center justify-center gap-[4px]`,
         'hover:shadow-sm hover:shadow-gray-200 transition duration-300',
         linkColor,
         borderColor,
