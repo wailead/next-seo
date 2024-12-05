67d9fc1fce49d5551e51dfdb0569ae49d6b50c62
lumpen <swk5373@gmail.com>
2024-12-05 15:45:01 +0900

    ⚙ Setting: husky commit message 룰 변경 #52

--- .husky/verifyCommitMessage.js
+++ .husky/verifyCommitMessage.js
@@ -1,22 +1,23 @@
 #!/usr/bin/env node
-const fs = require("fs");
+const fs = require('fs')
 
 // 커밋 메시지 파일 경로를 가져옵니다.
-const commitMessageFilePath = process.argv[2];
+const commitMessageFilePath = process.argv[2]
 
 // 커밋 메시지를 읽어옵니다.
-const commitMessage = fs.readFileSync(commitMessageFilePath, "utf-8").trim();
+const commitMessage = fs.readFileSync(commitMessageFilePath, 'utf-8').trim()
 
 // 병합 커밋 메시지 검증 제외
-if (commitMessage.startsWith("Merge branch")) {
-  process.exit(0); // 병합 커밋 메시지는 검증을 건너뜁니다.
+if (commitMessage.startsWith('Merge branch')) {
+  process.exit(0) // 병합 커밋 메시지는 검증을 건너뜁니다.
 }
 
 // 정규 표현식 수정 (커밋 메시지 형식을 검증)
-const commitMessagePattern = /^(Feat|Fix|Style|Design|HOTFIX|Refactor|Docs|Chore|Lint|Deploy|Rename|Remove|Type|Comment|Struct|SEO|Setting):\s.{1,40}\s#\d+$/;
+const commitMessagePattern =
+  /^(Feat|Fix|Style|Design|HOTFIX|Refactor|Docs|Chore|Lint|Deploy|Rename|Remove|Type|Comment|Structure|SEO|Setting):\s.{1,40}\s#\d+$/
 
-// 이모지 제거: 이모지가 있다면 제거하고, 나머지 텍스트만 검증 
-const sanitizedCommitMessage = commitMessage.replace(/^[^\w\s]+/, "").trim(); // 이모지 부분을 제거
+// 이모지 제거: 이모지가 있다면 제거하고, 나머지 텍스트만 검증
+const sanitizedCommitMessage = commitMessage.replace(/^[^\w\s]+/, '').trim() // 이모지 부분을 제거
 
 // 커밋 메시지 형식을 검증합니다.
 if (!commitMessagePattern.test(sanitizedCommitMessage)) {
@@ -39,14 +40,14 @@ if (!commitMessagePattern.test(sanitizedCommitMessage)) {
   Remove:         파일 삭제
   Type:           타입 수정
   Comment:        주석 추가 및 변경
-  Struct:         폴더 구조 변경
+  Structure:         폴더 구조 변경
   SEO:            웹 접근성
   ==================================================================
   형식에 맞춰 커밋 메시지를 다시 작성해주세요. 커밋 메시지는 설명 뒤에 이슈 번호도 포함해야 합니다. 예시: Feat: 기능 추가 설명 #123
   커밋 메시지는 총 80자로 제한됩니다. 가능한 간결한 커밋 메시지를 작성해 주세요.
-  `);
-  process.exit(1); // 검증 실패 시 프로세스를 종료합니다.
+  `)
+  process.exit(1) // 검증 실패 시 프로세스를 종료합니다.
 }
 
 // 성공적으로 완료되면 프로세스를 종료합니다.
-process.exit(0);
+process.exit(0)
