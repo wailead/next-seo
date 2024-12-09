3258a229d84f00d5bbbecac26f62c2c85043205a
양혜지 <97512098+hyeji17@users.noreply.github.com>
2024-12-10 03:04:51 +0900

    🎨 Style: Introduction 이미지 경ㄹ 수정, px -> rem, vw 변경 #93

--- src/app/components/title-form/TitleForm.tsx
+++ src/app/components/title-form/TitleForm.tsx
@@ -9,10 +9,10 @@ interface titleFormProps {
 
 function TitleForm({ subtitle, title, children, pb = 'pb-bottom' }: titleFormProps) {
   return (
-    <div className={`px-x pt-top ${pb} flex flex-col gap-[29.12px]`}>
-      <div className="flex flex-col gap-[6.24px]">
-        <p className="font-montserrat font-normal text-[14.56px] text-primary-default">{subtitle}</p>
-        <p className="font-pretendard font-bold text-[24.96px] text-gray-600">{title}</p>
+    <div className={`px-x pt-top ${pb} flex flex-col gap-[1.82rem]`}>
+      <div className="flex flex-col gap-[0.39rem]">
+        <p className="font-montserrat font-normal text-[0.91rem] text-primary-default">{subtitle}</p>
+        <p className="font-pretendard font-bold text-[1.56rem] text-gray-600">{title}</p>
       </div>
       {children}
     </div>
