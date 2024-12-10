b06ff33d2c0b1ec96329c87254c2bc1e9cde78ed
양혜지 <97512098+hyeji17@users.noreply.github.com>
2024-12-10 19:22:27 +0900

    ✨ Feat: 서비스 소개 컴포넌트 UI 구현 #95

--- src/app/(route)/(advertiser)/page.tsx
+++ src/app/(route)/(advertiser)/page.tsx
@@ -1,10 +1,12 @@
 import CompanyBanner from './_view/company-banner/CompanyBanner'
 import GovermentBanner from './_view/goverment-banner/GovermentBanner'
 import MoreService from './_view/more-service/MoreService'
+import ServiceIntroduction from './_view/service-introduction/ServiceIntroduction'
 
 function Advertiser() {
   return (
     <div>
+      <ServiceIntroduction />
       <CompanyBanner />
       <GovermentBanner />
       <MoreService />
