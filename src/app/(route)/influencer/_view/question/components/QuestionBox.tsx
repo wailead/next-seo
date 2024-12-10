80f68dc6de3cd9eb8c49c633c781d3ecdab0ba6b
양혜지 <97512098+hyeji17@users.noreply.github.com>
2024-12-10 17:26:20 +0900

    ✨ Feat: Question transition 속성 추가 #93

--- src/app/(route)/influencer/_view/question/components/QuestionBox.tsx
+++ src/app/(route)/influencer/_view/question/components/QuestionBox.tsx
@@ -1,30 +1,36 @@
 'use client'
 
-import { useState } from 'react'
+import { useRef, useEffect, useState } from 'react'
 import { Chevron } from '../../../../../../../public/icons'
 
 interface QuestionBoxProps {
   question: string
   answer: string | { text: string; link?: string }
+  isOpen: boolean
+  onToggle: () => void
 }
 
-function QuestionBox({ question, answer }: QuestionBoxProps) {
-  const [isToggle, setIsToggle] = useState<boolean>(false)
+function QuestionBox({ question, answer, isOpen, onToggle }: QuestionBoxProps) {
+  const contentRef = useRef<HTMLDivElement>(null)
+  const [height, setHeight] = useState(0)
 
-  const toggleOpen = (): void => {
-    setIsToggle(!isToggle)
-  }
+  useEffect(() => {
+    if (contentRef.current) {
+      setHeight(isOpen ? contentRef.current.scrollHeight : 0)
+    }
+  }, [isOpen])
 
   return (
     <div className="px-[1.235rem] py-[1.137rem] bg-white-200 rounded-[1.04rem] flex flex-col gap-[0.78rem]">
       <div className="flex justify-between items-center">
-        <p className="font-pretendard font-medium text-[0.91rem] text-gray-600">Q. {question}</p>
-        <div className="cursor-pointer" onClick={toggleOpen}>
-          <Chevron rotate={isToggle ? 180 : 0} color={isToggle ? '#2EC8C8' : '#222222'} />
+        <p className="font-pretendard font-medium text-[0.91rem] leading-[1.365rem] text-gray-600">Q. {question}</p>
+        <div className="cursor-pointer" onClick={onToggle}>
+          <Chevron rotate={isOpen ? 180 : 0} color={isOpen ? '#2EC8C8' : '#222222'} />
         </div>
       </div>
-      {isToggle && (
-        <>
+
+      <div className="transition-[max-height] duration-500 linear overflow-hidden" style={{ maxHeight: `${height}px` }}>
+        <div ref={contentRef} className="flex flex-col gap-[0.78rem]">
           <hr className="bg-white-400 h-[0.13rem]" />
           <p className="font-pretendard font-normal text-[#757575] text-[0.78rem]">
             {typeof answer === 'string' ? (
@@ -40,8 +46,8 @@ function QuestionBox({ question, answer }: QuestionBoxProps) {
               </>
             )}
           </p>
-        </>
-      )}
+        </div>
+      </div>
     </div>
   )
 }
