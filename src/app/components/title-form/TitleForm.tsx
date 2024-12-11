5d7ea8337ab31a65d7e3ee0ef87e60bb5752fa76
양혜지 <97512098+hyeji17@users.noreply.github.com>
2024-12-11 15:02:51 +0900

    ✨ Feat: TitleForm css 속성 변경 #101

--- src/app/components/title-form/TitleForm.tsx
+++ src/app/components/title-form/TitleForm.tsx
@@ -5,14 +5,18 @@ interface titleFormProps {
   title: string
   children?: ReactNode
   pb?: string
+  className?: string
 }
 
-function TitleForm({ subtitle, title, children, pb = 'pb-bottom' }: titleFormProps) {
+function TitleForm({ subtitle, title, children, pb = 'pb-bottom', className }: titleFormProps) {
   return (
-    <div className={`px-x pt-top ${pb} flex flex-col gap-[1.82rem]`}>
+    <div className={`px-x pt-top ${pb} flex flex-col gap-[1.82rem] ${className}`}>
       <div className="flex flex-col gap-[0.39rem]">
         <p className="font-montserrat font-normal text-[0.91rem] text-primary-default">{subtitle}</p>
-        <p className="font-pretendard font-bold text-[1.56rem] text-gray-600">{title}</p>
+        <p
+          className="font-pretendard font-bold text-[1.56rem] text-gray-600"
+          dangerouslySetInnerHTML={{ __html: title }}
+        />
       </div>
       {children}
     </div>
