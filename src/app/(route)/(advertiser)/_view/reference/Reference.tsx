9f494c3bbf5ed31de46ddb9ad885a0befc932216
Bomjin2 <opjkl4006@naver.com>
2024-12-29 17:08:34 +0900

    ✨ Feat: 광고주 페이지 반응형 구현 #156

--- src/app/(route)/(advertiser)/_view/reference/Reference.tsx
+++ src/app/(route)/(advertiser)/_view/reference/Reference.tsx
@@ -37,16 +37,16 @@ function Reference() {
     <div className="w-full  bg-primary-default ">
       <SectionLayout>
         <div
-          className={`flex h-[39.125rem] overflow-hidden justify-between ${selectTitle === 'more' ? 'pr-0' : 'pr-[1.25rem]'} `}>
+          className={`flex primary:h-[39.125rem] h-[83.47vw] overflow-hidden justify-between ${selectTitle === 'more' ? 'pr-0' : 'primary:pr-[1.25rem] pr-[2.67vw]'} `}>
           <TitleForm
             title={`와이리 인플루언서 마케팅\n 사례들을 확인해보세요`}
             subtitle="REFERENCE"
             titleColor="text-white-default"
             subTitleColor="text-white-default"
             pb="0"
-            pr="pl-[3.25rem] pr-0">
-            <div className="flex flex-col justify-between gap-[1.82rem]">
-              <div className="flex flex-col  gap-[1.25rem] ">
+            pr="primary:pl-[3.25rem] pl-[6.93vw] pr-0">
+            <div className="flex flex-col justify-between primary:gap-[1.82rem] gap-[3.88vw]">
+              <div className="flex flex-col  primary:gap-[1.25rem] gap-[2.67vw]  primary:h-[10.438rem] h-[22.27vw]">
                 <ReferenceCard
                   title="와이리 X 평창 휘닉스파크"
                   id="pyeongchang"
@@ -76,14 +76,14 @@ function Reference() {
                   descNum=""
                 />
               </div>
-              <div className="relative w-[23.75rem] h-[16.563rem] items-end">
+              <div className="relative primary:w-[23.75rem] w-[50.67vw] primary:h-[16.563rem] h-[35.33vw] items-end">
                 {selectTitle !== 'more' && (
                   <Image
                     src={`/images/advertiser/peference/${selectTitle}/${selectTitle}_main.png`}
                     alt={`${selectTitle}`}
                     width={380}
                     height={264}
-                    className="absolute left-[-1.125rem]"
+                    className="absolute primary:left-[-1.125rem] left-[-2.4vw] primary:bottom-[0.875rem] bottom-[1.86vw]"
                   />
                 )}
               </div>
