350db655e365594e544b9584e245ea5e7a846bb7
양혜지 <97512098+hyeji17@users.noreply.github.com>
2025-01-05 19:33:23 +0900

    🎨 Design: About 컴포넌트 반응형 UI 구현 #161

--- src/app/components/title-form/TitleForm.tsx
+++ src/app/components/title-form/TitleForm.tsx
@@ -10,6 +10,7 @@ interface titleFormProps {
   titleColor?: string
   subTitleColor?: string
   className?: string
+  gap?: string
 }
 
 function TitleForm({
@@ -21,11 +22,12 @@ function TitleForm({
   titleColor,
   subTitleColor,
   className,
+  gap = 'primary:gap-[1.82rem] gap-[3.87vw]',
 }: titleFormProps) {
   return (
     <div
       className={twMerge(
-        `primary:px-x px-[6.93vw] primary:pt-top pt-[6.24vw] ${pb} flex flex-col primary:gap-[1.82rem] gap-[3.87vw] ${className}`,
+        `primary:px-x px-[6.93vw] primary:pt-top pt-[6.24vw] ${pb} flex flex-col ${gap} ${className}`,
         pr
       )}>
       <div className="flex flex-col primary:gap-[0.39rem] gap-[0.83vw]">
