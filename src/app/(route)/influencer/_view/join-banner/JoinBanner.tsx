58f2cf13d7715ddcb8040567dfb89f6bd3bf4354
lumpen <swk5373@gmail.com>
2025-01-06 17:03:16 +0900

    ✨ Feat: 인플루언서 페이지 반응형 구현 - 가입하기 배너 #141

--- src/app/(route)/influencer/_view/join-banner/JoinBanner.tsx
+++ src/app/(route)/influencer/_view/join-banner/JoinBanner.tsx
@@ -1,16 +1,12 @@
-import BackgroundImageBox from './components/background-image-box'
 import JoinBox from './components/JoinBox'
 import PhoneImageBox from './components/phone-image-box'
 
 function JoinBanner() {
   return (
-    <section className="h-[21.25rem] w-full bg-primary-200 relative">
-      <BackgroundImageBox />
-      <div className="absolute top-0 left-0 w-full h-full z-10 flex justify-center">
-        <div className="w-primary h-full flex justify-center items-center gap-2">
-          <JoinBox />
-          <PhoneImageBox />
-        </div>
+    <section className="h-[45vw] primary:h-[21.25rem] w-full bg-primary-200 flex justify-center bg-[url('/assets/images/background_bubble.png')] bg-[length:100vw_100%] primary:bg-[length:800px_100%] bg-[60%_center] bg-repeat-x">
+      <div className="w-primary h-full flex justify-center items-center gap-2">
+        <JoinBox />
+        <PhoneImageBox />
       </div>
     </section>
   )
