b5f03f5fb04d27babd3dcbf1c2b6aa9a20c3e14d
양혜지 <97512098+hyeji17@users.noreply.github.com>
2025-01-07 22:19:04 +0900

    🎨 Design: Performance 컴포넌트 모바일 버전 UI 구현 #191

--- src/app/(route)/(advertiser)/_view/performance/Performance.tsx
+++ src/app/(route)/(advertiser)/_view/performance/Performance.tsx
@@ -5,20 +5,24 @@ function Performance() {
   return (
     <div className="bg-[#FFF9ED]">
       <SectionLayout>
-        <div className="px-x pt-top pb-bottom flex flex-col gap-[0.39rem]">
-          <p className="font-montserrat font-normal text-[0.91rem] text-primary-default">PERFORMANCE</p>
-          <div className="flex justify-between">
-            <p className="font-pretendard font-bold primary:text-[1.56rem] text-[3.33vw] text-gray-600">
+        <div className="mobile:px-x px-[6.67vw] mobile:pt-top pt-[8.89vw] mobile:pb-bottom pb-[16.67vw] flex flex-col gap-[0.39rem]">
+          <p className="font-montserrat font-normal primary:text-[0.91rem] mobile:text-[1.94vw] text-[4.17vw] text-primary-default">
+            PERFORMANCE
+          </p>
+          <div className="flex mobile:flex-row flex-col mobile:justify-between mobile:gap-0 gap-[11.11vw]">
+            <p className="font-pretendard font-bold primary:text-[1.56rem] mobile:text-[3.33vw] text-[6.67vw] text-gray-600">
               와이리를 통해 캠페인을
               <br />
               진행 해보세요
             </p>
-            <div className="flex justify-center">
-              <div className="grid grid-cols-2 grid-rows-2 primary:gap-y-[2.34rem] gap-y-[5vw] primary:gap-x-[3.413rem] gap-x-[7.29vw] text-center">
+            <div className="flex mobile:justify-center">
+              <div className="w-full grid grid-cols-2 grid-rows-2 primary:gap-y-[2.34rem] mobile:gap-y-[5vw] gap-y-[6.67vw] primary:gap-x-[3.413rem] mobile:gap-x-[7.29vw] justify-between text-center">
                 <PerformanceBox title="보유 인플루언서 수" count="25,000+" unit="명" />
-                <div></div>
+                <div />
                 <PerformanceBox title="캠페인 신청수" count="16,024+" unit="건" />
-                <PerformanceBox title="캠페인 선정수" count="2,281+" unit="건" />
+                <div className="col-start-2 row-start-2 justify-items-end">
+                  <PerformanceBox title="캠페인 선정수" count="2,281+" unit="건" />
+                </div>
               </div>
             </div>
           </div>
