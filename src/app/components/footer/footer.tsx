6a7c24b786b10426588386fbde7f80b086cb8947
Bomjin2 <opjkl4006@naver.com>
2025-01-08 15:25:30 +0900

    ✨ Feat: Footer 모바일 UI 구현 #212

--- src/app/components/footer/footer.tsx
+++ src/app/components/footer/footer.tsx
@@ -3,27 +3,28 @@ import CustomImage from '../custom-image/CustomImage'
 function Footer() {
   return (
     <>
-      <div className="flex w-full primary:h-[8.375rem] h-[17.87vw] bg-gray-600 items-end justify-center border-t-2 border-gray-300 ">
+      <div className="flex w-full primary:h-[8.375rem] mobile:h-[17.87vw] bg-gray-600 items-end justify-center border-t-2 border-gray-300 ">
         <div
-          className={`flex w-primary items-end justify-center primary:pt-[1.688rem] pt-[3.6vw] primary:pb-[1.5rem] pb-[3.2vw] primary:px-[7.5rem] px-[16vw] primary:gap-[3rem] gap-[6.4vw]`}>
+          className={`flex mobile:flex-row flex-col w-primary mobile:items-end justify-center primary:pt-[1.688rem] mobile:pt-[3.6vw] pt-[5.56vw] primary:pb-[1.5rem] mobile:pb-[3.2vw] pb-[5.56vw] primary:px-[7.5rem] mobile:px-[16vw] px-[13.89vw] primary:gap-[3rem] mobile:gap-[6.4vw] gap-[2.78vw]`}>
           <div className={`flex flex-col primary:gap-[0.188rem] gap-[0.4vw]`}>
-            <div className={`flex items-center justify-start primary:gap-3 gap-[1.6vw]`}>
+            <div className={`flex items-center justify-start primary:gap-3 mobile:gap-[1.6vw] gap-[2.78vw]`}>
               <CustomImage
                 src="/images/footerlogo.png"
                 alt="Logo"
-                w="primary:w-[3.75rem] w-[8vw]"
-                h="primary:h-[1.625rem] h-[3.47vw]"
+                w="primary:w-[3.75rem] mobile:w-[8vw] w-[15.56vw]"
+                h="primary:h-[1.625rem] mobile:h-[3.47vw] h-[8.33vw]"
               />
-              <span className={`font-medium primary:text-[0.719rem] text-[1.53vw] text-white-default `}>
+              <span
+                className={`font-medium primary:text-[0.719rem] mobile:text-[1.53vw] text-[3.89vw] text-white-default `}>
                 (주) 와이리
               </span>
             </div>
             <div
-              className={`flex text-gray-200 font-normal primary:text-[0.6rem] text-[1.28vw] primary:leading-[0.538rem] leading-[1.15vw]`}>
+              className={` text-gray-200 font-normal primary:text-[0.6rem] text-[1.28vw] primary:leading-[0.538rem] leading-[1.15vw] mobile:flex hidden`}>
               <p className="flex flex-col primary:gap-[0.406rem] gap-[0.87vw] font-pretendard ">
                 <span className="block">대표자 : 김범석</span>
                 <span className="block">
-                  사업자등록번호 : 441-86-02504&nbsp;&nbsp;|&nbsp;&nbsp;통신판매업신고 : 2021-서울영등포-2197
+                  사업자등록번호 : 441-86-02504 &nbsp;&nbsp;|&nbsp;&nbsp;통신판매업신고 : 2021-서울영등포-2197
                 </span>
                 <span className="block">주소 : 서울시 동작구 상도로 55길 6, 테크스테이션 408호</span>
                 <span className="block">
@@ -31,21 +32,38 @@ function Footer() {
                 </span>
               </p>
             </div>
+            <div className="mobile:hidden flex">
+              <p className="text-gray-200 font-normal whitespace-pre-line text-[3.33vw]">
+                {`대표자: 김범석
+                사업자 등록번호 : 441-86-02504
+                통신판매업신고 : 2021-서울영등포-2197
+                주소 : 서울시 동작구 상도로 55길 6, 
+                테크스테이션 408호
+                이메일 : wairi_sales@naver.com
+                대표전화 : 070-8098-7127`}
+              </p>
+            </div>
           </div>
-          <div className={`flex flex-col primary:w-[8.563rem] w-[18.27vw] primary:gap-2 gap-[1.07vw] `}>
-            <div className="flex flex-col primary:gap-[0.813rem] gap-[1.73vw] ">
+          <div
+            className={`flex mobile:flex-col primary:w-[8.938rem] mobile:w-[19vw] primary:gap-2 mobile:gap-[1.07vw] gap-[11.11vw]`}>
+            <div className="flex flex-col primary:gap-[0.813rem] mobile:gap-[1.73vw] ">
               <span
-                className={`font-medium text-white-default primary:text-[0.719rem] text-[1.53vw] primary:leading-[0.5rem] leading-[1.07vw]`}>
+                className={`mobile:font-medium text-white-default primary:text-[0.719rem] mobile:text-[1.53vw] text-[3.89vw] primary:leading-[0.5rem] mobile:leading-[1.07vw]`}>
                 와이리 고객센터
               </span>
               <span
-                className={`font-bold text-white-default primary:text-[1rem] text-[2.13vw] primary:leading-[0.563rem] leading-[1.2vw]`}>
+                className={`mobile:font-bold text-white-default primary:text-[1rem] mobile:text-[2.13vw] text-[3.89vw] primary:leading-[0.563rem] mobile:leading-[1.2vw] `}>
                 070-8098-7127
               </span>
             </div>
-            <div className={`flex primary:leading-[0.875rem] leading-[1.87vw] primary:gap-1 gap-[1.07vw]`}>
-              <span className={`font-normal  text-white-default primary:text-[0.75rem] text-[1.6vw]`}>운영시간</span>
-              <span className={`font-normal text-white-default primary:text-[0.75rem] text-[1.6vw]`}>
+            <div
+              className={`flex mobile:flex-row flex-col primary:leading-[0.875rem] mobile:leading-[1.87vw] primary:gap-1 mobile:gap-[1.07vw]`}>
+              <span
+                className={`font-normal text-white-default primary:text-[0.75rem] mobile:text-[1.6vw] text-[3.89vw]`}>
+                운영시간
+              </span>
+              <span
+                className={`font-normal text-white-default primary:text-[0.75rem] mobile:text-[1.6vw] text-[3.89vw]`}>
                 평일 10:00~18:00
               </span>
             </div>
